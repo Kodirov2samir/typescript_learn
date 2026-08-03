a5a7392fbef01aa2787d80f915c322c3d137e2a8
Kodirov2samir <kodirovsamir174@gmail.com>
2026-08-03 15:04:59 +0500

    subtype supertype

--- ts-practice/index.ts
+++ ts-practice/index.ts
@@ -9,7 +9,30 @@ console.log(numExplicit);
 // console.log(numTestExplicit); //ide imdiately gives an error
 
 //Structural typization
-//Structural typing in TypeScript means that type compatibility is determined solely by the shape or structure of the data, rather than by explicit declarations or type names
+//Structural typing in TypeScript means that type compatibility is determined solely by the shape or structure of the data, rather than by explicit declarations or type names:
+interface User {
+  id: string;
+  name: string;
+}
+
+interface Customer {
+  id: string;
+  name: string;
+}
+
+// 2. Initialize a variable with the 'User' type
+let currentUser: User = { id: "USR-101", name: "Alice" };
+
+// 3. Assign it to a 'Customer' variable
+// This works perfectly because their structures are identical!
+let activeCustomer: Customer = currentUser;
+
+function printId(obj: { id: string }) {
+  console.log(obj.id);
+}
+
+// 4. This also works because both types contain an 'id' property
+printId(currentUser);
 
 //Data types
 //data types in ts are the same as in js but there are special types
@@ -67,3 +90,21 @@ type FullInfo = AdditionalInfo & MainInfo;
 const info0: FullInfo = { firstname: "Samir", lastname: "Kodirov", age: 20 };
 console.log(info0); //{ firstname: 'Samir', lastname: 'Kodirov', age: 20 }
 //so it means an object should include all the fields
+
+//Supertype Subtype
+//Subtype is a type that includes all the parents fields plus have own
+type SuperType = {
+  name: string;
+};
+type SubType = {
+  name: string;
+  age: number; //includes both part filed(name) and own field(age)
+};
+//as typization in ts is structural we can interchange the types; we can do:
+const subType: SubType = { name: "Samir", age: 20 };
+const superType: SuperType = subType; //correct because supertype requires to have alt least naem and subType has is
+/**
+ * const supeType: SuperType = { name: "Samir"};
+const subType: SubType = superType;//incorrect because although subType includes name, it laso requires age but there is no age
+ */
+console.log(superType);
